d06e96b98302c8e5f62b9959006d1a24147ed038
jacobnachman <iowafan1997@gmail.com>
2017-10-21 10:28:58 -0500

    Added working fragments to switch to signup from signin

--- app/src/main/java/com/hackathon/dadbod/dadbod/LoginActivity.java
+++ app/src/main/java/com/hackathon/dadbod/dadbod/LoginActivity.java
@@ -1,128 +1,40 @@
 package com.hackathon.dadbod.dadbod;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.app.FragmentManager;
+import android.support.v4.app.FragmentActivity;
+
 
 /**
  * Created by iowaf on 10/20/2017.
  */
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends FragmentActivity implements FragmentChangeListener {
 
-    private FirebaseAuth mAuth;
-
-    private FirebaseAuth.AuthStateListener mAuthListener;
-
-    private static final String TAG = "SignInActivity";
-
-    private Button signIn;
-
-    private EditText email;
-
-    private EditText password;
+    private FragmentManager fm;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    Log.d(TAG, "onAuthStateChanged:signed_out:");
-                }
-            }
-        };
+        fm = getFragmentManager();
+        FragmentTransaction ftransaction = fm.beginTransaction();
 
-        email = (EditText) findViewById(R.id.etEmail);
-        password = (EditText) findViewById(R.id.etPassword);
-        signIn =(Button) findViewById(R.id.blogin);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                createAccount(email.getText().toString(), password.getText().toString());
-            }
-        });
+        ftransaction.add(R.id.contentFragment, new FragmentSignIn());
+        ftransaction.commit();
     }
 
     @Override
-    public void onStart(){
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    public void onStop(){
-        super.onStop();
-        if(mAuthListener != null){
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-
-    private void createAccount(String email, String password){
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-
-                        //sign in fail
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, R.string.auth_failed, Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                });
-    }
-
-    private void signIn(String email, String password){
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-                        if(!task.isSuccessful()){
-                            Log.w(TAG, "signInWithEmail:failed", task.getException());
-                            Toast.makeText(LoginActivity.this, R.string.auth_failed, Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-    private void getUserInfo(FirebaseUser user){
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null){
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-
-            //Do not use this to authenticate with backend server, use FirebaseUser.getToken() instead
-            String uid = user.getUid();
+    public void replaceFragment(Activity_Tags activityTag) {
+        FragmentTransaction ftransaction = fm.beginTransaction();
+        if(activityTag == Activity_Tags.FRAGMENT_SIGN_UP){
+            ftransaction.replace(R.id.contentFragment, new FragmentSignUp());
+            ftransaction.commit();
         }
     }
 }

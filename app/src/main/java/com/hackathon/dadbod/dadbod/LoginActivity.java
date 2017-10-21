c82e9ae8dca7f1ce7913706bd9b8d2ed0864ca7b
jacobnachman <iowafan1997@gmail.com>
2017-10-21 01:14:15 -0500

    Email Login and Password with Firebase

--- app/src/main/java/com/hackathon/dadbod/dadbod/LoginActivity.java
+++ app/src/main/java/com/hackathon/dadbod/dadbod/LoginActivity.java
@@ -1,11 +1,23 @@
 package com.hackathon.dadbod.dadbod;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -22,14 +34,27 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private static final String TAG = "";
+    private static final String TAG = "SignInActivity";
+
+    private static final int RC_SIGN_IN = 9001;
+
+    private Button signIn;
+
+    private Button gSignIn;
+
+    private EditText email;
+
+    private EditText password;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private TextView mStatusTextView;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         mAuth = FirebaseAuth.getInstance();
-
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -41,6 +66,18 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         };
+
+        email = (EditText) findViewById(R.id.editText);
+        password = (EditText) findViewById(R.id.editText2);
+        password = (EditText) findViewById(R.id.editText2);
+        signIn =(Button) findViewById(R.id.signup);
+        gSignIn = (Button) findViewById(R.id.g_sign_in_button);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount(email.getText().toString(), password.getText().toString());
+            }
+        });
     }
 
     @Override
@@ -71,6 +108,31 @@ public class LoginActivity extends AppCompatActivity {
 
                     }
                 });
+    }
 
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        if(!task.isSuccessful()){
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, R.string.auth_failed, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void getUserInfo(FirebaseUser user){
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            //Do not use this to authenticate with backend server, use FirebaseUser.getToken() instead
+            String uid = user.getUid();
+        }
     }
 }
--- app/src/main/java/com/hackathon/dadbod/dadbod/MainActivity.java
+++ app/src/main/java/com/hackathon/dadbod/dadbod/MainActivity.java
@@ -1,16 +1,8 @@
 package com.hackathon.dadbod.dadbod;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
